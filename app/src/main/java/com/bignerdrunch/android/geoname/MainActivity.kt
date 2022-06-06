16798f54acb4b0e2f9457592c651b2e5fb636bae
Alexey-Lysenko <63449694+Alexey-Lysenko@users.noreply.github.com>
2022-06-06 16:11:12 +0700

    added override lifecycle activity functions with logger

--- app/src/main/java/com/bignerdrunch/android/geoname/MainActivity.kt
+++ app/src/main/java/com/bignerdrunch/android/geoname/MainActivity.kt
@@ -2,11 +2,13 @@ package com.bignerdrunch.android.geoname
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private val questionBank = listOf(Question(R.string.question_australia,true),
@@ -41,6 +43,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG,"onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -64,9 +67,36 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
         questionTextView.setOnClickListener { nextButton.callOnClick() }
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume() called")
+    }
 
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy() called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG,"onSaveInstanceState() called")
     }
 }
 
