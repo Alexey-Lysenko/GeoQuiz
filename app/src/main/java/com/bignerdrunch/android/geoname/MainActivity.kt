9eac9ee5ef41020bd9992a7e91206aeeb179f4dd
Alexey-Lysenko <63449694+Alexey-Lysenko@users.noreply.github.com>
2022-06-11 20:00:27 +0700

    Linked main activity and cheat activity

--- app/src/main/java/com/bignerdrunch/android/geoname/MainActivity.kt
+++ app/src/main/java/com/bignerdrunch/android/geoname/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bignerdrunch.android.geoname
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: ImageButton
     private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
+    private lateinit var cheatButton: Button
 
     private fun updateQuestion(){
         val questionTextResId = quizViewModel.currentQuestionText
@@ -38,9 +40,9 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show()
-        if (quizViewModel.currentIndex == quizViewModel.questionBank.size-1) {
+        if (quizViewModel.currentIndex == quizViewModel.questionBankSize-1) {
             Toast.makeText(this, "${
-                quizViewModel.correctAnswerCount.toDouble() / quizViewModel.questionBank.size.toDouble() *100
+                quizViewModel.correctAnswerCount.toDouble() / quizViewModel.questionBankSize.toDouble() *100
             }%",
                 Toast.LENGTH_LONG).show()
             quizViewModel.correctAnswerCount = 0
@@ -59,6 +61,7 @@ class MainActivity : AppCompatActivity() {
         nextButton = findViewById(R.id.next_button)
         prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
+        cheatButton = findViewById(R.id.cheat_button)
 
         trueButton.setOnClickListener {
             checkAnswer(true)
@@ -89,6 +92,12 @@ class MainActivity : AppCompatActivity() {
             nextButton.callOnClick()
             trueButton.isEnabled = true
             falseButton.isEnabled = true
+        }
+
+        cheatButton.setOnClickListener {
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity,answerIsTrue)
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/com/bignerdrunch/android/geoname/QuizViewModel.kt
+++ app/src/main/java/com/bignerdrunch/android/geoname/QuizViewModel.kt
@@ -4,13 +4,14 @@ import androidx.lifecycle.ViewModel
 
 
 class QuizViewModel: ViewModel() {
-    val questionBank = listOf(Question(R.string.question_australia,true),
+    private val questionBank = listOf(Question(R.string.question_australia,true),
                               Question(R.string.question_oceans,true),
                               Question(R.string.question_mideast,false),
                               Question(R.string.question_africa,false),
                               Question(R.string.question_americas,true),
                               Question(R.string.question_asia,true)
     )
+    val questionBankSize = questionBank.size
     var prevIndex = 0
     var correctAnswerCount = 0
     var currentIndex = 0
@@ -24,3 +25,5 @@ class QuizViewModel: ViewModel() {
         currentIndex = (currentIndex + 1) % questionBank.size
     }
 }
+
+
